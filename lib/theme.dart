551cf62e303f23a21b983d0e2163cd888d0267a2
Arcod7 <antoine.esman@epitech.eu>
2024-04-09 15:28:36 +0200

    [ADD] colors

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData().copyWith(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xFF4A148C),
+    secondary: const Color(0xFF8f8fff),
+    // secondaryContainer: const Color(0xFFe3e3f1),
+    secondaryContainer: const Color(0xFFd9d9fc),
+    surfaceVariant: const Color(0xFFe3e3f1),
+    // secondaryContainer: const Color(0xFFaeaed7),
+  ),
+);
