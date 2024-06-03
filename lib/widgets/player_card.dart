c3950451f577a4bf542369d17781cf23b095390f
sambrus <samuel.bruschet@gmail.com>
2024-06-03 08:00:06 +0200

    feat: player card ongoing

--- lib/widgets/player_card.dart
+++ lib/widgets/player_card.dart
@@ -6,15 +6,49 @@ class PlayerCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      //color: Theme.of(context).colorScheme.primary,
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         splashColor: Colors.blue.withAlpha(30),
         onTap: () {},
-        child: const Column(
-          children: [
-            Text("hello"),
-          ],
-        ),
+        child: SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(25, 5, 0, 5),
+              child: Column(
+                children: [
+                  const Text(
+                    "Carte de \"Status\"",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: double.infinity,
+                    height: 180,
+                    child: Row(
+                      children: [
+                         Container(
+                          color: Colors.yellow,
+                          height: 180,
+                          width: 220,
+                          child: const Column(
+                            children: [
+                              Text("salut"),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(),
+                  Row(),
+                ],
+              ),
+            )),
       ),
     );
   }
