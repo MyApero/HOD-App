d51b840df52fd5f3a961fc9045449a114ef408cb
Samuel <samuel.bruschet@gmail.com>
2024-06-07 12:33:36 +0200

    feat: main screen for every poles

--- lib/features/werewolf/screens/main_werewolf_screen.dart
+++ lib/features/werewolf/screens/main_werewolf_screen.dart
@@ -3,24 +3,24 @@ import 'package:hod_app/features/games/screens/widget/event_details_screen.dart'
 import 'package:hod_app/widgets/background/app_scaffold.dart';
 import 'package:hod_app/widgets/select_button.dart';
 
-class MainRoleGameScreen extends StatelessWidget {
-  const MainRoleGameScreen({super.key});
+class MainWereWolfScreen extends StatelessWidget {
+  const MainWereWolfScreen({super.key});
 
-  static route() => MaterialPageRoute(builder: (ctx) => MainRoleGameScreen());
+  static route() => MaterialPageRoute(builder: (ctx) => MainWereWolfScreen());
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      hasBackArrow: true,
-        title: "Pole jeux de rôles",
+        hasBackArrow: true,
+        title: "Loup-garou",
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            SelectButton(label: "Sessions de jeux de rôle"),
+            SelectButton(label: "Rôles"),
             SelectButton(
               label: "Events details",
               onPressed: () => Navigator.of(context).push(
-                EventDetailsScreen.route(title: "Jeux de rôle", pole: "toudoulelou"),
+                EventDetailsScreen.route(title: "Loup-garou", pole: "werewolf"),
               ),
             )
           ],
