a5b737ab15ed20660b53dcf071f7bcf61a6d2dea
Samuel <samuel.bruschet@gmail.com>
2024-06-11 19:27:37 +0200

    fix: event card displaying

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hod_app/features/auth/widgets/firebase_auth.dart';
 import 'package:hod_app/theme/theme.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'firebase_options.dart';
 
 void main() async {
