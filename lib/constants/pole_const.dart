c7b30457d1f77d1038b5fa83327f8a367b84f438
Samuel <samuel.bruschet@gmail.com>
2024-06-11 19:27:37 +0200

    fix: event card displaying

---
+++ lib/constants/pole_const.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Map poleNamesMap = {
+  'rolegame': 'Jeu de rôle',
+  'boardgame': 'Jeu de société',
+  'chess': 'Réflexion',
+  'tradingcardgame' : 'Trading Card Game',
+  'wargame': 'Jeu de guerre',
+  'werewolf': 'Loup-Garou',
+  'toudoulelou': 'Tout doux le louppppp'
+};
