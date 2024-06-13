3ecb01df6cee16558ebe2147bbe0126aa2332232
Samuel <samuel.bruschet@gmail.com>
2024-06-13 13:41:14 +0200

    fix: events displaying by ascending date

--- lib/features/home/view/home.dart
+++ lib/features/home/view/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hod_app/features/home/view/widgets/event_card.dart';
 import 'package:hod_app/features/home/view/widgets/event_data.dart';
+import 'package:hod_app/models/event_model.dart';
 import 'package:hod_app/widgets/simple_text.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,6 +19,12 @@ class HomeScreen extends StatelessWidget {
           const SimpleText("Les prochains événements"),
           EventData(
             builder: (events) {
+          events.sort((a,b) {
+    var adate = a.startDate;
+     var bdate = b.startDate;
+     return adate.compareTo(bdate);
+  });
+;
               return Expanded(
                 child: GridView.builder(
                   itemCount: events.length,
